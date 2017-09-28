dd55b10255f93f0f29e9821edba9dc1e15aaa70a
Jérémy Pinsolle <jpinsolle@xebia.fr>
2017-09-28 13:53:44 +0200

    Demo

--- java-example/src/main/java/fr/xebia/HandlerHTTP.java
+++ java-example/src/main/java/fr/xebia/HandlerHTTP.java
@@ -5,10 +5,8 @@ import com.amazonaws.serverless.proxy.internal.model.AwsProxyResponse;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import fr.xebia.code.AwsProxyResponseBuilder;
-import org.apache.log4j.Logger;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Optional;
 
 public class HandlerHTTP implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
